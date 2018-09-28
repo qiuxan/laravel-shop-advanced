0d70da0b4f30affd5c2a4eb907297bb9414908b1
qiuxan <qiuxan@gmail.com>
2018-09-28 06:55:42 +0000

    init categate

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -24,5 +24,12 @@ class Product extends Model
             return $this->attributes['image'];
         }
         return \Storage::disk('public')->url($this->attributes['image']);
+
+
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
     }
 }
